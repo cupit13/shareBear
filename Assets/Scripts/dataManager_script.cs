d9556be6bd6fa7b1d0c82f1e37148c2eccf78e41
unknown <jeff.johar@gmail.com>
2019-07-21 09:24:22 -0700

    added persistent manager practice file fetching

---
+++ Assets/Scripts/dataManager_script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class dataManager_script : MonoBehaviour
+{
+    public string username;
+    public Text fie_username;
+    
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("manager");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void loadScene(int scnInd)
+    {
+        SceneManager.LoadScene(scnInd);
+    }
+
+    public void updateName()
+    {
+        username = fie_username.text;
+    }
+}
---
+++ Assets/Scripts/loadDataManager_script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadDataManager_script : MonoBehaviour
+{
+    dataManager_script dataScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dataScript = GameObject.Find("dataManager").GetComponent<dataManager_script>();
+        welcomeNameUpdate();
+    }
+
+    void welcomeNameUpdate()
+    {
+        GetComponent<Text>().text = "Welcome \n" + dataScript.username;
+    }
+}
